19a7ad310cc9b7874bbe14c4b6d26210c434baf1
rensyuudk <pbenkyouu@gmail.com>
2026-05-11 07:05:20 +0700

    update

---
+++ frontend/app/kursus/uiux/page.tsx
@@ -0,0 +1,67 @@
+"use client"
+
+import "@/app/globals.css"
+
+export default function Uiux() {
+    return (
+        <div className="flex flex-col min-h-screen bg-white">
+            <header className="fixed top-0 left-0 w-full h-[92px] md:h-[92px] // bg-white flex items-center pl-[20px] md:pl-[70px] border-b-[4px] border-[#7E7F97]">
+                        <a href=""><img src="/logo.png" alt="logo" className="w-[67px] h-[67px] // md:w-[92px] md:h-[92px] object-contain"/></a>
+                <nav className="flex flex-row items-center pt-[12px] pb-[13px] // md:pt-0 pb-0">
+                        <div className="flex">
+                          <button className="md: ml-[650px] md: pt-[8px] md: pb-[8px] md: pl-[29px] md: pr-[29px] // border-1 rounded-[15px] border-white hover:bg-[#7E7F97] cursor-pointer">Kursus</button>
+                          <button className="md: ml-[77px] md: pt-[8px] md: pb-[8px] md: w-[150px] // border-1 rounded-[15px] border-white hover:bg-[#7E7F97] object-contain cursor-pointer">Tentang Kami</button>
+                        </div>
+
+                        <div className=" md: mt-[10px] md: h-[60px] md: w-[60px] md: ml-[650px] // flex bg-[#D9D9D9] rounded-full items-center justify-center cursor-pointer">
+                        <p>F</p>
+                        </div>
+                </nav>
+            </header>
+
+            <main className="md: mt-[140px] //">
+            <div className="md: ml-[300px] md: mr-[300px]">
+                <p className="md: text-[40px] md: ml-[40px]">UI/UX</p>
+                <hr />
+            </div>
+
+            <div>
+                <details>
+                    <summary>
+                        <span></span>
+                    </summary>
+                    <div>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </details>
+
+                <details>
+                    <summary>
+                        <span></span>
+                    </summary>
+                    <div>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </details>
+            </div>
+
+            <div className="md: pt-[100px] flex flex-col md: ml-[450px] md: mr-[450px] ">
+            </div>
+            </main>
+
+            <footer className="w-full h-[400px] border-t-[4px] border-[#7E7F97] mt-[56px] flex flex-col md:flex-row">
+                <div className="pl-[40px] pt-[30px] md:pl-[70px] md:pt-[20px]">
+                    <img src="/logo.png" alt="logo" className="w-[66px] h-[66px] md:w-[78px] md:h-[78px]"/>
+                    <p className="text-[#5C5858] text-[19px] md:text-[17px] pt-[20px] md:pt-[20px]">Belajar bersama di kursku</p>
+                </div>
+
+                <div className="pl-[40px] pt-[50px] md:pt-[39px] md:pl-[103px]">
+                    <h3 className="font-bold text-[21px] md:text-[20px]">Tentang kami</h3>
+                    <a href=""><p className="text-[19px] md:text-[18px] pt-[40px] md:pt-[30px]">tentang kursku</p></a>
+                </div>
+            </footer>
+        </div>
+    )
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,7 +7,7 @@ export default function Loginpage() {
        <div className="flex flex-col min-h-screen bg-white">
             <header className="fixed top-0 left-0 w-full h-[92px] md:h-[92px] bg-white flex items-center pl-[20px] md:pl-[70px] border-b-[4px] border-[#7E7F97]">
                 <nav className="flex flex-col items-center pt-[12px] pb-[13px] md:pt-0 pb-0">
-                        <a href=""><img src="/logo.png" alt="logo" className="w-[67px] h-[67px] md:w-[92px] md:h-[92px] object-contain"/></a>
+                        <img src="/logo.png" alt="logo" className="w-[67px] h-[67px] md:w-[92px] md:h-[92px] object-contain"/>
                 </nav>
             </header>
 
@@ -26,23 +26,10 @@ export default function Loginpage() {
                     
                     <button type="submit" className="bg-[#125E9C] pt-[14px] pb-[13px] rounded-[15px] mt-[33px] text-white border-1 border-black cursor-pointer hover:bg-[#133C5D] md:text-[20px]">Masuk</button>
 
-                    <div className="flex justify-center pt-[30px] md:text-[20px]">
-                        <p>Belum ada akun? <a href="" className="text-[#6781D9] hover:text-[#213578]">Daftar</a></p>
-                    </div>
                 </form>
             </main>
 
-            <footer className="w-full h-[400px] border-t-[4px] border-[#7E7F97] mt-[56px] flex flex-col md:flex-row">
-                <div className="pl-[40px] pt-[30px] md:pl-[70px] md:pt-[20px]">
-                    <img src="/logo.png" alt="logo" className="w-[66px] h-[66px] md:w-[78px] md:h-[78px]"/>
-                    <p className="text-[#5C5858] text-[19px] md:text-[17px] pt-[20px] md:pt-[20px]">Belajar bersama di kursku</p>
-                </div>
-
-                <div className="pl-[40px] pt-[50px] md:pt-[39px] md:pl-[103px]">
-                    <h3 className="font-bold text-[21px] md:text-[20px]">Tentang kami</h3>
-                    <a href=""><p className="text-[19px] md:text-[18px] pt-[40px] md:pt-[30px]">tentang kursku</p></a>
-                </div>
-            </footer>
+            <footer className="w-full h-[400px] border-t-[4px] border-[#7E7F97] mt-[56px] flex flex-col md:flex-row"></footer>
         </div>
     )
 }
