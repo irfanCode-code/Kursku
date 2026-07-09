9edbcfda138bd286c41c9a2ab5cdf33a05bc8cff
rensyuudk <pbenkyouu@gmail.com>
2026-07-09 19:50:13 +0700

    feat: add submission controllers

---
+++ backend/controllers/submission.go
@@ -0,0 +1,203 @@
+package controllers
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/irfanCode-code/kursku/backend/config"
+	"github.com/irfanCode-code/kursku/backend/models"
+)
+
+func CreateSubmission(c fiber.Ctx) error {
+	modulIDstr := c.FormValue("modul_id")
+	siswaIDstr := c.FormValue("siswa_id")
+
+	if modulIDstr == "" || siswaIDstr == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "modul id dan siswa id wajib diisi",
+		})
+	}
+
+	var modulID, siswaID uint
+	_, _ = fmt.Scanf(modulIDstr, "%d", &modulID)
+	_, _ = fmt.Scanf(siswaIDstr, "%d", siswaID)
+
+	var modul models.Modul
+	if err := config.DB.First(&modul, modulID).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "modul tidak ditemukan",
+		})
+	}
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "file .pdf wajib diunggah",
+		})
+	}
+
+	ext := filepath.Ext(file.Filename)
+	if ext != ".pdf" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "file harus berformat .pdf",
+		})
+	}
+
+	uploadDir := "./uploads/submissions"
+	if err := os.MkdirAll(uploadDir, os.ModePerm); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "gagal menyiapkan folder penyimpanan di server",
+		})
+	}
+
+	uniqueFileName := fmt.Sprintf("tugas_%d_user_%d_%d%s", modulID, siswaID, time.Now().Unix(), ext)
+	filePath := filepath.Join(uploadDir, uniqueFileName)
+
+	if err := c.SaveFile(file, filePath); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "gagal menyimpan file jawaban ke server",
+		})
+	}
+
+	newSubmission := models.Submission{
+		FileUrl: filePath,
+		Grade:   0.0,
+		ModulID: modulID,
+		SiswaID: siswaID,
+	}
+
+	if err := config.DB.Create(&newSubmission).Error; err != nil {
+		os.Remove(filePath)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "gagal mengirimkan tugas ke database",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "berhasil mengirimkan tugas",
+		"data":    newSubmission,
+	})
+}
+
+func UpdateSubmission(c fiber.Ctx) error {
+	submissionID := c.Params("id")
+	var submission models.Submission
+
+	if err := config.DB.First(&submission, submissionID).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "tugas tidak ditemukan",
+		})
+	}
+
+	gradeStr := c.FormValue("grade")
+	if gradeStr != "" {
+		var newGrade float32
+		_, err := fmt.Sscanf(gradeStr, "%f", newGrade)
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": "format harus berupa angka desimal",
+			})
+		}
+		submission.Grade = newGrade
+	}
+
+	file, err := c.FormFile("file")
+	if err == nil {
+		ext := filepath.Ext(file.Filename)
+		if ext != ".pdf" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": "format tidak mendukung! harus .pdf",
+			})
+		}
+
+		uploadDir := "./uploads/submissions"
+		uniqueFileName := fmt.Sprintf("tugas_update_%d_%s", time.Now().Unix(), file.Filename)
+		newFilePath := filepath.Join(uploadDir, uniqueFileName)
+		if err := c.SaveFile(file, newFilePath); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": "gagal update file",
+			})
+		}
+		if submission.FileUrl != "" {
+			_ = os.Remove(submission.FileUrl)
+		}
+		submission.FileUrl = newFilePath
+	}
+
+	if err := config.DB.Save(&submission).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "gagal memperbarui tugas ke database",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "berhasil memperbarui tugas",
+		"data":    submission,
+	})
+}
+
+func DeleteSubmission(c fiber.Ctx) error {
+	SubmissionID := c.Params("id")
+	var submission models.Submission
+
+	if err := config.DB.First(&submission, SubmissionID).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "data tugas tidak ditemukan",
+		})
+	}
+
+	if submission.FileUrl != "" {
+		err := os.Remove(submission.FileUrl)
+		if err != nil {
+			fmt.Printf("gagal menghapus file", err)
+		}
+	}
+
+	if err := config.DB.Unscoped().Delete(&submission).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "gagal menghapus file tugas",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "file tugas berhasil dihapus",
+	})
+}
+
+func GetSubmissionByModul(c fiber.Ctx) error {
+	modulID := c.Params("modul_id")
+	var submission []models.Submission
+
+	err := config.DB.Preload("siswa").Where("modul_id = ?", modulID).Find(&submission).Error
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "gagal mengambil tugas yang dikumpulkan",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "berhasil mengambil tugas yang dikumpulkan",
+		"total":   len(submission),
+		"data":    submission,
+	})
+}
+
+func GetSubmissionById(c fiber.Ctx) error {
+	submissionID := c.Params("id")
+	var submission models.Submission
+
+	err := config.DB.Preload("siswa").Preload("modul").First(&submission, submissionID).Error
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "tugas tidak ditemukan",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "berhasil mengambil tugas",
+		"data":    submission,
+	})
+}
